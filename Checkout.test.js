eb280042949ad16605e7ba33a596183f68cd9597
andy-hammond <andrew.hammond@yahoo.com>
2018-06-29 15:51:11 +0100

    fixing the error to show ramesh

--- Checkout.test.js
+++ Checkout.test.js
@@ -8,7 +8,7 @@ const testBaskets = [
         items: [
             {id: "A", quantity: 1}, {id: "B", quantity: 1}, {id: "C", quantity: 1}
         ],
-        result: 101
+        result: 100
     },
     {
         description: "testing basket #2 (B, A, B, A, A)",
